971cf2df45f1ea7c7ac4423b3c31dcf5dbbad9e2
AgosAcosta <agosacosta38@gmail.com>
2023-12-13 17:56:41 -0300

    Botones

--- 20231213-1-Acosta-Proyecto Completo Angular/EjecitacionAngular/src/app/estructura/main/body/body.component.ts
+++ 20231213-1-Acosta-Proyecto Completo Angular/EjecitacionAngular/src/app/estructura/main/body/body.component.ts
@@ -8,9 +8,20 @@ import { Component } from '@angular/core';
 export class BodyComponent {
   input = '';
   tareas: { nombre: string; completa: boolean }[] = [];
-  
+
   agregarLista() {
     this.tareas.push({ nombre: this.input, completa: false });
     this.input = '';
   }
+
+  eliminarTarea(index: number) {
+    this.tareas.splice(index, 1);
+  }
+  TareasPendientes() {
+    this.tareas = this.tareas.filter((tarea) => !tarea.completa);
+  }
+  TareasCompletas() {
+    this.tareas = this.tareas.filter((tarea) => tarea.completa);
+  }
+  TareasEliminadas() {}
 }
